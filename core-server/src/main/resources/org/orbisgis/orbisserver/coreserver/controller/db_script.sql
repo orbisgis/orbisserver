214c7aa5060197a2312ba38f47adef7e8c39415e
Palominos Sylvain <sylvain.palominos@univ-ubs.fr>
2017-07-12 16:09:05 +0200

    Finish the login modal.

--- core-server/src/main/resources/org/orbisgis/orbisserver/coreserver/controller/db_script.sql
+++ core-server/src/main/resources/org/orbisgis/orbisserver/coreserver/controller/db_script.sql
@@ -1,4 +1,4 @@
 -- Script of the initiation of the database.
-DROP TABLE IF EXISTS USER;
-CREATE TABLE USER (username VARCHAR(50), password VARCHAR(50));
-INSERT INTO USER VALUES ('admin', 'admin');
+DROP TABLE IF EXISTS users_table;
+CREATE TABLE users_table (username VARCHAR(50), password VARCHAR(50));
+INSERT INTO users_table VALUES ('admin', 'admin');
